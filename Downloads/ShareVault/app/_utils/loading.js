8595ae00550eac89ddac68cd3f8c977b841b2911
Aayush Singh <97270649+Aayu-shSingh@users.noreply.github.com>
2025-02-22 17:39:51 +0530

    Delete Downloads/ShareVault directory

--- Downloads/ShareVault/app/_utils/loading.js
+++
@@ -1,8 +0,0 @@
-export const withLoadingDelay = (callback, delay = 500) => {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      callback();
-      resolve();
-    }, delay);
-  });
-}; 
